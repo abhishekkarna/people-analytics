eb0500554fa0b7b7e5fdcdba9d1e3a29d80d5484
Abhishek Mani <amani@expertinstitute.com>
2025-02-04 11:35:37 +0530

    bugfix - Fixed minor bug - Removed swagger boilerplate

--- talent_checkin_backend/config/database.ts
+++ talent_checkin_backend/config/database.ts
@@ -4,7 +4,7 @@ const username = process.env.db_username || "";
 const password = process.env.db_password || "";
 const host = process.env.db_host || "";
 const dialect = "postgres";
-const database = process.env.db_username || "";
+const database = process.env.db_database || "";
 
 export const DB_CONFIG: Config = {
   development: {
--- talent_checkin_backend/src/index.ts
+++ talent_checkin_backend/src/index.ts
@@ -3,14 +3,11 @@
 import express from "express";
 import cors from "cors";
 import compression from "compression";
-import swaggerJSDoc from "swagger-jsdoc";
-import swaggerUi from "swagger-ui-express";
 import dotenv from "dotenv";
 dotenv.config();
 import { sequelize } from "@db/models/index";
 import associations from "@db/models/associations";
 import Logger from "@utils/logger";
-import swaggerDefinition from "@docs/apidoc";
 import correlator from "express-correlation-id";
 import { Application } from "express";
 import { apiRoutes } from "./routes";
@@ -38,10 +35,6 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"), false);
     }
   },
-};
-const swaggerOptions = {
-  swaggerDefinition,
-  apis: ["./src/routes/*.ts"], // Adjust for TypeScript files
 };
 process.on("SIGINT", () => {
   logger.log("stopping the server", "info");
@@ -75,22 +68,7 @@ app.use(compression());
 app.use("/api/v1", apiRoutes);
 app.use(errorHandler);
 
-// const swaggerSpec = swaggerJSDoc(swaggerOptions);
-// if (
-//   process.env.NODE_ENV === "development" ||
-//   process.env.NODE_ENV === undefined
-// ) {
-//   app.use(
-//     "/docs",
-//     swaggerUi.serve,
-//     swaggerUi.setup(swaggerSpec, {
-//       swaggerOptions: { persistAuthorization: true },
-//     })
-//   );
-// }
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
 export default app;
